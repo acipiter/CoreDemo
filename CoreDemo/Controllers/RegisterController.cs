8b65f8a519d50314121b3595b934dd75e901b7df
acipiter <shngndgd@hotmail.com>
2021-11-10 11:29:32 +0300

    33. Ders, Şifre eşleşme ödevi

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.WriterAbout = "Yazar hakkında";
+            wm.AddWriter(writer);
+            //var values = writer;
+            return RedirectToAction("Blog","Blog");
+        }
+    }
+}
